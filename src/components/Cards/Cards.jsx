002d4ee9341d28ad8650a77900f85241cd82973d
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-09-02 11:40:11 -0500

    Implemented the graph component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 import styles from './Cards.module.css';
+import cx from 'classnames';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   console.log(confirmed);
@@ -13,7 +14,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify='center'>
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
           <CardContent>
             <Typography color='textSecondary' gutterBottom>
               Infected
@@ -38,7 +45,13 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
           <CardContent>
             <Typography color='textSecondary' gutterBottom>
               Recovered
@@ -64,15 +77,31 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
         </Grid>
 
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
           <CardContent>
             <Typography color='textSecondary' gutterBottom>
               Deaths
             </Typography>
 
-            <Typography variant='h5'>REAL DATA</Typography>
+            <Typography variant='h5'>
+              {' '}
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=','
+              ></CountUp>
+            </Typography>
 
-            <Typography color='textSecondary'>REAL DATE</Typography>
+            <Typography color='textSecondary'>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
 
             <Typography variant='body2'>
               Number of death caused by COVID-19
